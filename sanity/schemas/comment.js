55eac3364a233459e2998ea8e8b46f80a1e453db
Nader Elhaj <naderelhaj3@gmail.com>
2022-06-23 16:49:48 +0100

    Setting up sanity.io

---
+++ sanity/schemas/comment.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'string',
+    },
+    {
+      name:"username",
+      title:"username",
+      type:"string"
+    },
+    {
+      name:"profileImg",
+      title:"Profile Image",
+      type:"string"
+    }
+  ]
+}
